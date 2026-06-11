be0877c771f67ca8fa2656c5a7956595e670874e
EMSLRMVA <scrap.emslrmva@gmail.com>
2026-06-11 16:33:10 +0530

    Create firebase_options.dart

---
+++ lib/firebase_options.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyBpUG-fd_Xzo1IQ38piGaqen7cn2o_Cps4',
+    authDomain: 'scrap-management-system-20bf7.firebaseapp.com',
+    projectId: 'scrap-management-system-20bf7',
+    storageBucket: 'scrap-management-system-20bf7.firebasestorage.app',
+    messagingSenderId: '152198928623',
+    appId: '1:152198928623:web:4490a6c5b699eddda5a31a',
+    measurementId: 'G-DV946CWYEM',
+  );
+}
